dcf3a91c74f79202646cd1ccfae0a448c1c737cc
tonpe-narayana <chalirahul37@gmail.com>
2021-11-29 14:35:16 +0530

    first commit

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ListComponent} from './list/list.component';
+import {LoginComponent} from './login/login.component';
+console.warn('user module console')
+
+const routes: Routes = [
+  {path:'list',component:ListComponent},
+  {path:'login',component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
